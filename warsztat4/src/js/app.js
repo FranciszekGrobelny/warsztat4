11b8cd03c9066fdaa09a1148d95ef291ddaa617d
FranciszekGrobelny <48532425+FranciszekGrobelny@users.noreply.github.com>
2020-02-23 14:35:15 +0100

    initiall commit

---
+++ warsztat4/src/js/app.js
@@ -0,0 +1,36 @@
+$(function () {
+        // var currentDtaeContainer = $('.current-date');
+        // var DATE_API = 'http://date.jsontest.com';
+        //
+        // function fetchDate(){
+        //     $.ajax({
+        //         url: DATE_API,
+        //         type: 'GET'
+        //     }).done(function (result) {
+        //         currentDtaeContainer
+        //             .text(result.time+", "+ result.date);
+        //     });
+        // }
+        //
+        // fetchDate();
+        //
+        // var starWarsContainer = $('.star-wars');
+        // var PEOPLE_API = 'https://swapi.co/api';
+        // var vader = '/people/88'
+        // function fetchStarWars(){
+        //     $.ajax({
+        //         url: PEOPLE_API + vader,
+        //         type: 'GET'
+        //     }).done(function (result) {
+        //         renderPerson(result)
+        //     });
+        // }
+        // fetchStarWars();
+        //
+        // function renderPerson(person){
+        //     var name = $('<strong>').text(person.name);
+        //     starWarsContainer.append(name);
+        // }
+
+    var booksName = $();
+});
