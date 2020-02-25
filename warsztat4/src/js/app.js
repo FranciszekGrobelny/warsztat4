647399a198239e64b320b454b2a646e50c8b0561
FranciszekGrobelny <48532425+FranciszekGrobelny@users.noreply.github.com>
2020-02-25 21:59:45 +0100

    added buttons to delefe books from server

--- warsztat4/src/js/app.js
+++ warsztat4/src/js/app.js
@@ -1,61 +1,86 @@
 $(function () {
-        // var currentDtaeContainer = $('.current-date');
-        // var DATE_API = 'http://date.jsontest.com';
-        //
-        // function fetchDate(){
-        //     $.ajax({
-        //         url: DATE_API,
-        //         type: 'GET'
-        //     }).done(function (result) {
-        //         currentDtaeContainer
-        //             .text(result.time+", "+ result.date);
-        //     });
-        // }
-        //
-        // fetchDate();
-        //
-        // var starWarsContainer = $('.star-wars');
-        // var PEOPLE_API = 'https://swapi.co/api';
-        // var vader = '/people/88'
-        // function fetchStarWars(){
-        //     $.ajax({
-        //         url: PEOPLE_API + vader,
-        //         type: 'GET'
-        //     }).done(function (result) {
-        //         renderPerson(result)
-        //     });
-        // }
-        // fetchStarWars();
-        //
-        // function renderPerson(person){
-        //     var name = $('<strong>').text(person.name);
-        //     starWarsContainer.append(name);
-        // }
-
     var booksNameContainer = $('.books');
-    var BOOKS_API = 'http://localhost:8282/books';
-    var books;
+    var BOOKS_API = 'http://localhost:8282/books/';
+    var addBookForm = $('#addBook');
+    var submitButton = $('#addBook button');
+    var Book = function(isbn, title, author, publisher, type){
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.type = type;
+    };
 
-    function fetchBooks(){
-        $.ajax({
-            url: BOOKS_API,
-            type:'GET'
-        }).done(function (result) {
-            books = result;
-            Array.from(result).forEach(function (e) {
-                var title = $('<h2 class="title"></h2>').text(e.title);
-                var div = $('<div class="description"></div>');
-                div.text(e.id).slideUp();
-                booksNameContainer.append(title);
-                title.after(div);
+    fetchBooks(BOOKS_API,booksNameContainer);
+    deleteSubmit(addBookForm);
 
-                title.on('click',function () {
-                    div.slideToggle();
+    submitButton.on('click',function () {
+        var newBook = new Book(
+            $('#addBook input:nth-of-type(1)').val(),
+            $('#addBook input:nth-of-type(2)').val(),
+            $('#addBook input:nth-of-type(3)').val(),
+            $('#addBook input:nth-of-type(4)').val(),
+            $('#addBook input:nth-of-type(5)').val());
 
-                });
-            })
-        });
-    }
-    fetchBooks();
+        sendBookToServer(BOOKS_API,newBook);
+        fetchBooks(BOOKS_API,booksNameContainer);
 
+    });
+
+    deleteBook(BOOKS_API,booksNameContainer);
+    fetchBooks(BOOKS_API,booksNameContainer);
 });
+//-----------------------functions-----------------------------------------------------------
+function fetchBooks(api,container){
+    $('.books').children().remove();
+    $.ajax({
+        url: api,
+        type:'GET'
+    }).done(function (result) {
+        Array.from(result).forEach(function (e) {
+            var title = $('<h2 class="title"></h2>').text(e.title);
+            var div = $('<div class="description"></div>');
+            div.text(e.id).slideUp();
+            container.append(title);
+            title.append(div);
+            addButton(title);
+            title.on('click',function () {
+                div.slideToggle();
+            });
+        });
+    });
+}
+
+function deleteSubmit(btn){
+    btn.on('submit', function (e) {
+        e.preventDefault();
+    });
+}
+
+function sendBookToServer(api,book){
+    $.ajax({
+        url: api,
+        data: JSON.stringify(book),
+        type:'POST',
+        dataType: 'json',
+        contentType: 'application/json'
+    });
+}
+
+function ajaxDelete(api,el){
+    $.ajax({
+        url: api+el,
+        type:'DELETE'
+    });
+}
+
+function deleteBook(api,container){
+    container.on('click', '.btn',function () {
+        ajaxDelete(api,$(this).prev().children().eq(0).text());
+        fetchBooks(api,container);
+    });
+}
+
+function addButton(el){
+    el.after('<button class="btn">Delete book</button>');
+}
